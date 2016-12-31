10b1ebf9fd724e4e3322c013c088d79746ec1099
fillu87gyc <wander.off65@gmail.com>
2017-01-01 02:48:27 +0900

    なんかかわったっけ

--- ConsoleApplication35/Program.cs
+++ ConsoleApplication35/Program.cs
@@ -1,5 +1,26 @@
 ﻿using System;
 
+/*
+ * Receiving 0
+ * Div by Zero
+ * Leave try
+ * 
+ * Receiving 1
+ * out of range
+ * Leave try
+ * 
+ * Receiving 2
+ * Leave try
+ * 
+ * Receiving 3
+ * Leave try
+ * 
+ * Receiving 4
+ * Leave try
+ * 
+ * 続行するには何かキーを押してください . . .
+ */
+
 class Program
 {
 	static void Main()
